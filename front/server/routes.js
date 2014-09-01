0d020e844cee92748eafe37c94b484b7d9424bd7
Thomas Porter <thomporter@gmail.com>
2014-09-01 17:23:52 -0400

    removed depreciated endpoint

--- front/server/routes.js
+++ front/server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/checklists', require('./api/checklist'));
-  app.use('/api/activities', require('./api/activity'));
   app.use('/api/logs', require('./api/log'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
