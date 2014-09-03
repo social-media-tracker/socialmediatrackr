e2822fc5ffeae41566826484fe14a8dcf686b886
Thomas Porter <thomporter@gmail.com>
2014-09-03 03:41:53 -0400

    adding logo & front banner to home page (#14)

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -350,6 +350,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            // 'assets/**/*',
             'index.html'
           ]
         }, {
