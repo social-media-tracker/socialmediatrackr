190bf82d785476fd40627cbf6a66257a6aec8c70
Thomas Porter <thomporter@gmail.com>
2014-09-07 04:40:56 -0400

    added ReplySchema to activity log model

--- front/server/api/log/log.model.js
+++ front/server/api/log/log.model.js
@@ -1,13 +1,27 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    trackable = require('mongoose-trackable');
+var mongoose      = require('mongoose')
+  , Schema        = mongoose.Schema
+  , ObjectId      = mongoose.Schema.Types.ObjectId
+  , trackable     = require('mongoose-trackable')
+;
+
+
+var ReplySchema = new Schema({
+  message: {type: String, required: true},
+  user: {type: Number, ref: 'User'}
+});
+
+ReplySchema.plugin(trackable);
 
 var LogSchema = new Schema({
   message: String,
   user: {type: Number, ref: 'User'},
-  checklist: {type: Schema.Types.ObjectId, ref: 'Checklist', required: false}
+  checklist: {type: Schema.Types.ObjectId, ref: 'Checklist', required: false},
+  uploadKey: String,
+  attachments:[{type: ObjectId, ref: 'Attachment'}],
+  replies: [ReplySchema],
+
 });
 
 LogSchema.plugin(trackable);
